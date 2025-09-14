e722094561bd4b883931fbde14e15865fe45284a
Karandeep Singh <singhkarandeep6970@gmail.com>
2025-09-15 03:10:19 +0530

    Time: 0 ms (100%), Space: 44.6 MB (85.77%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,27 +13,49 @@
  *     }
  * }
  */
+
+// o(n)
 class Solution {
+    public int ans = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        return diam(root);
+        height(root);
+        return ans;
     }
 
-    public static int diam(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int ld = diam(root.left);
-        int rd = diam(root.right);
-        int currd = height(root.left) + height(root.right);
-        return Math.max(ld, (Math.max(rd, currd)));
-    }
-
-    public static int height(TreeNode root) {
+    public int height(TreeNode root) {
         if (root == null) {
             return 0;
         }
         int lh = height(root.left);
         int rh = height(root.right);
+        ans = Math.max(ans, lh + rh);
         return Math.max(lh, rh) + 1;
     }
 }
+
+// n*n approach 
+
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         return diam(root);
+//     }
+
+//     public static int diam(TreeNode root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int ld = diam(root.left);
+//         int rd = diam(root.right);
+//         int currd = height(root.left) + height(root.right);
+//         return Math.max(ld,(Math.max(rd,currd)));
+//     }
+//     public static int height(TreeNode root){
+//         if(root == null){
+//             return 0;
+//         }
+//         int lh = height(root.left);
+//         int rh = height(root.right);
+//         return Math.max(lh,rh)+1;
+//     }
+// }
